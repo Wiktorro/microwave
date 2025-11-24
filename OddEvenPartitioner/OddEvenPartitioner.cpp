4411699806f7572a33e9d4e328c148af4aae72c9
Wiktorro <mwiktor023@gmail.com>
2025-11-24 07:37:34 +0100

    declare initial OddEvenPartitioner classes

--- OddEvenPartitioner/OddEvenPartitioner.cpp
+++ OddEvenPartitioner/OddEvenPartitioner.cpp
@@ -1,4 +1,22 @@
 
 //
+#include <iostream>
+#include <vector>
 
+struct  INumberProcessor
+{
+virtual ~INumberProcessor() = default;
+virtual std::vector<int> process(const std::vector<int>& input) = 0;
+};
+
+struct OddEvenPartitioner: public INumberProcessor
+{
+virtual ~OddEvenPartitioner() = default;
+virtual std::vector<int> process(const std::vector<int>& input) override;
+};
+
+std::vector<int> OddEvenPartitioner::process(const std::vector<int>& input)
+{
+  return {};
+}
 //

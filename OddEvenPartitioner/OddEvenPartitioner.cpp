298871e1e8c7db809b8eb9b2c21f1559b2031738
Wiktor Mis <mwiktor023@gmail.com>
2025-11-26 17:58:46 +0100

    Implement main function for OddEvenPartitioner with testing

--- OddEvenPartitioner/OddEvenPartitioner.cpp
+++ OddEvenPartitioner/OddEvenPartitioner.cpp
@@ -27,3 +27,20 @@ You can name your implementation class however you like (for example, OddEvenPar
 //
 
 //
+
+int main()
+{
+    OddEvenPartitioner p;
+    std::vector<int> input{4, 1, 3, 2, 8, 5};
+    const auto ret = p.process(input);
+    std::cout<<"Hello World\ninput\n";
+    for(const int val : input)
+        std::cout<<val << ' ';
+    std::cout<<"\n output\n";
+    for(const int val : ret)
+        std::cout<<val << ' ';
+    
+
+    return 0;
+}
+

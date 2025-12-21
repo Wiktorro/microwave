90c9f823bb505a6423476f1d49c8dfb096e6ac59
Wiktor Mis <mwiktor023@gmail.com>
2025-12-21 13:21:53 +0100

    add working solution

--- num2textConverter/solution.cpp
+++ num2textConverter/solution.cpp
@@ -18,9 +18,24 @@ virtual std::vector<int> process(const std::vector<int>& input) override;
 
 std::vector<int> OddEvenPartitioner::process(const std::vector<int>& input)
 {
-  
-    return {};
+    std::vector<int> output (input.size());
+    auto iter = output.begin();
     
+    for(const int val : input)
+        if(1 == val %2)
+        {
+            *iter = val;
+            ++iter;
+        }
+                
+    for(const int val : input)
+        if(0 == val %2)
+        {
+            *iter = val;
+            ++iter;
+        }
+            
+    return output;
 }
 //
 

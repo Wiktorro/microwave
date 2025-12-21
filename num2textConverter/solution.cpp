882ec1c67c6b1c654e2b0f2e83331c942fb2f402
Wiktor Mis <mwiktor023@gmail.com>
2025-12-21 13:38:23 +0100

    improve num2textConverter testing

--- num2textConverter/solution.cpp
+++ num2textConverter/solution.cpp
@@ -23,22 +23,34 @@ std::vector<int> OddEvenPartitioner::process(const std::vector<int>& input)
     
 }
 //
+OddEvenPartitioner p;
 
+// #define 
+void CHECK(std::vector<int> input  ){
+    const auto ret = p.process(input);  \
+    std::cout<<"Hello World\ninput\n";  \
+    for(const int val : input)           \
+        std::cout<<val << ' ';                  \
+    std::cout<<"\n output\n";                   \
+    for(const int val : ret)                    \
+        std::cout<<val << ' ';
+        }
 //
 // optymalizacja - zacznij iterowac jeszcze od konca, kopiuj parzyste az do do 1 npar
 // nastepne przejsce dowkrotne juz tylko od begin do zmniejszonego end
 
 int main()
 {
-    OddEvenPartitioner p;
-    std::vector<int> input{4, 1, 3, 2, 8, 5};
-    const auto ret = p.process(input);
-    std::cout<<"Hello World\ninput\n";
-    for(const int val : input)
-        std::cout<<val << ' ';
-    std::cout<<"\n output\n";
-    for(const int val : ret)
-        std::cout<<val << ' ';
+    CHECK({2,4, 7 , 0, 1, 99, 66, 3, 2, 8, 5});
+    CHECK({4, 1, 3, 2, 8, 5});
+    // std::vector<int> input{4, 1, 3, 2, 8, 5};
+    // const auto ret = p.process(input);
+    // std::cout<<"Hello World\ninput\n";
+    // for(const int val : input)
+    //     std::cout<<val << ' ';
+    // std::cout<<"\n output\n";
+    // for(const int val : ret)
+    //     std::cout<<val << ' ';
     
     return 0;
 }

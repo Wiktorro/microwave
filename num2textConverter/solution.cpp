6a80bdf56a6fac68eccd6a03912938b45c5eec25
Wiktor Mis <mwiktor023@gmail.com>
2025-12-21 13:25:16 +0100

    add num2textConverter testing

--- num2textConverter/solution.cpp
+++ num2textConverter/solution.cpp
@@ -25,5 +25,21 @@ std::vector<int> OddEvenPartitioner::process(const std::vector<int>& input)
 //
 
 //
+// optymalizacja - zacznij iterowac jeszcze od konca, kopiuj parzyste az do do 1 npar
+// nastepne przejsce dowkrotne juz tylko od begin do zmniejszonego end
 
+int main()
+{
+    OddEvenPartitioner p;
+    std::vector<int> input{4, 1, 3, 2, 8, 5};
+    const auto ret = p.process(input);
+    std::cout<<"Hello World\ninput\n";
+    for(const int val : input)
+        std::cout<<val << ' ';
+    std::cout<<"\n output\n";
+    for(const int val : ret)
+        std::cout<<val << ' ';
+    
+    return 0;
+}
 //

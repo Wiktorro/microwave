ed775b0e3b0ad9648545583bec5b4f3dad12a693
Wiktor Mis <mwiktor023@gmail.com>
2025-12-23 12:50:34 +0100

    implement text2num solution

--- text2numConverter/solution.cpp
+++ text2numConverter/solution.cpp
@@ -1,4 +1,41 @@
 
+size_t text2num(const char* str)
+{
+    if (nullptr == str || 0 == *str)
+    {
+        return 0;
+    }
+
+    int cnt=0 ,maxcnt =0;
+    char 
+    maxchar = 0, // just debug purpose var
+    last = *str;
+    auto iter = str +1;
+
+    while (0 != iter)
+    {
+        if (*iter == last)
+        {
+            ++cnt;
+        }
+        else
+        {
+            if (cnt > maxcnt)
+            {
+                maxchar = last;
+                maxcnt = cnt;
+                last = *iter;
+            }
+            
+        }
+        
+        
+        ++str;
+    }
+    
+
+    return 0;
+}
 
 //
 

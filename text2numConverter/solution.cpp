50b47cb3590de4f5a3d5f852b0f3940328834f21
Wiktor Mis <mwiktor023@gmail.com>
2025-12-23 12:55:30 +0100

    add num2textConverter testing

--- text2numConverter/solution.cpp
+++ text2numConverter/solution.cpp
@@ -5,3 +5,20 @@
 //
 
 //
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#define CHECK(x) cout << x << " <> " << (x) << endl
+
+int main(int argc, char const *argv[])
+{
+    CHECK("1234");
+    CHECK("12  34");
+    CHECK("87654");
+    CHECK("-7654");
+    
+    return 0;
+}
